e6e926a9d83c03a0918e866e8480b57fa58f57f5
Andres David serrano <ads.f117@gmail.com>
2018-06-03 19:53:28 -0500

    provides multiple server using @Provides @Named

--- app/src/main/java/puzzlebench/com/dagger2_kotlin_kata/ContactListActivity.kt
+++ app/src/main/java/puzzlebench/com/dagger2_kotlin_kata/ContactListActivity.kt
@@ -4,24 +4,33 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import dagger.Component
 import kotlinx.android.synthetic.main.activity_contact_list.*
-import puzzlebench.com.dagger2_kotlin_kata.di.ContactBag
+import puzzlebench.com.dagger2_kotlin_kata.di.ContactSurce
+import puzzlebench.com.dagger2_kotlin_kata.di.SERVER1
+import puzzlebench.com.dagger2_kotlin_kata.di.SERVER2
 import puzzlebench.com.dagger2_kotlin_kata.model.Contact
 import javax.inject.Inject
+import javax.inject.Named
 
 class ContactListActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var contact: Contact
+    @field:Named(SERVER1)
+    lateinit var contactFromServer1: Contact
+    @Inject
+    @field:Named(SERVER2)
+    lateinit var contatcFromServer2: Contact
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_list)
         DaggerContactMagicBox.create().inject(this)
-        contact_name_text_view.text = contact.contactName
+        contact_name_server1_text_view.text = contactFromServer1.contactName
+        contact_name_server2_text_view.text = contatcFromServer2.contactName
     }
 }
 
-@Component(modules = [ContactBag::class])
+@Component(modules = [ContactSurce::class])
 interface ContactMagicBox {
     fun inject(app: ContactListActivity)
 }
--- app/src/main/java/puzzlebench/com/dagger2_kotlin_kata/di/Module.kt
+++ app/src/main/java/puzzlebench/com/dagger2_kotlin_kata/di/Module.kt
@@ -3,11 +3,23 @@ package puzzlebench.com.dagger2_kotlin_kata.di
 import dagger.Module
 import dagger.Provides
 import puzzlebench.com.dagger2_kotlin_kata.model.Contact
+import javax.inject.Named
+
+
+const val SERVER1 = "Server1"
+const val SERVER2 = "Server2"
 
 @Module
-class ContactBag {
+class ContactSurce {
     @Provides
+    @Named(SERVER1)
     fun getContatcFromServiceV1(): Contact {
-        return Contact("Contact From Service Version 1")
+        return Contact("Contact From Service Version 1 Using Dagger2")
+    }
+
+    @Provides
+    @Named(SERVER2)
+    fun getContatcFromServiceV2(): Contact {
+        return Contact("Contact From Service Version 2 Using Dagger2")
     }
 }
